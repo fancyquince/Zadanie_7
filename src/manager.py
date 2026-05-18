92b0c1292832b48e91e6ce0621770c68d59e8424
fancyquince <igor.mlynarycz@gmail.com>
2026-05-18 23:47:04 +0200

    Dodano funkcjonalnosc B

--- src/manager.py
+++ src/manager.py
@@ -112,3 +112,13 @@ class Manager:
         if apartment_key not in self.apartments:
             raise ValueError("Apartment key does not exist")
         return any([bill for bill in self.bills if bill.apartment == apartment_key and bill.settlement_year == year and bill.settlement_month == month])
+    
+    def is_tenant_blacklisted(self, tenant_name: str) -> bool:
+        if getattr(self, 'blacklist', None) is None:
+            return False
+            
+        for osoba in self.blacklist:
+            if osoba.get("name") == tenant_name:
+                return True
+                
+        return False 
